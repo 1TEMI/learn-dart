8aad73df7ab1ce76e8e024739dbf27bbce57454a
1TEMI <omowayegbotemi@gmail.com>
2023-12-30 18:09:22 +0100

    I learn about Int,num and double

---
+++ 003_Int_Num_Double/int_num_double.dart
@@ -0,0 +1,17 @@
+void main(){
+  //int only take whole number
+  int value1 = 20;
+  int value2 = 10;
+
+  //Double can take whole number, decimal and negative
+  double score1 = 30.56;
+  double score2 = 10.15;
+
+  //Num can all type of number
+  num myValue = 25;
+  num myScore = 25.54;
+  num myDebt = -300;
+
+  // print(value1.toString().runtimeType); //using runtimeType to check the dataType
+  print(value1 + value2);
+}
