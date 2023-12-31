e49630e80fddca9d92341befd6fbe4840763ab45
1TEMI <omowayegbotemi@gmail.com>
2023-12-31 11:32:28 +0100

    Explore Sets in dart

---
+++ 007_Set/sets.dart
@@ -0,0 +1,7 @@
+void main(){
+  Set score = {1,2,2,3,4,5,6,7,8,9,10,'Missing Script'}; // Set only return a unique and it can take any datatype except define
+  print(score);
+
+  Set<String> names ={'Temi','Matthew','Tofunmi','Dare'}; // This set can only take string cause it was define
+  print(names);
+}
